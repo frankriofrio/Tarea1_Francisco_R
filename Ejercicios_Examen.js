33261a8610125f0c5829d5764b768fadfe3b7b7f
frankriofrio <44818518+frankriofrio@users.noreply.github.com>
2022-07-10 16:47:29 -0500

    Ejercicios Examen

---
+++ Ejercicios_Examen.js
@@ -0,0 +1,25 @@
+ //Pregunta 3
+let numero1 = [1,3,5,7,10,11]
+let numero1= [1,3,5]
+var resultado = myFunction(numero1);
+alert("El número "+numero+" es "+resultado);
+
+function myFunction(numero) {
+  if(numero % 2 == 0) {
+    return "true";
+  }
+  else {
+    return "false";
+  }
+}
+
+
+
+
+//Pregunta 6
+//Tabla de Multiplicar
+var tabla = 4;
+            for (var i = 0; i < 13; i++) {
+                console.log(""+tabla+" X "+i+" = "+(i*tabla));
+           }
+        
